905c6f2dd891aeee0454543c2b956fbd9ba4589b
lllh <1371911217@qq.com>
2019-02-23 19:17:30 +0800

    update

---
+++ Assets/Editor/Test1/Editor_Test1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Test1))]
+public class Editor_Test1 : Editor {
+
+	public override void OnInspectorGUI(){
+		Test1 test = target as Test1;
+
+		test.MRect = EditorGUILayout.RectField("rect title", test.MRect);
+		test.MTexture = EditorGUILayout.ObjectField("一个texture", test.MTexture, typeof(Texture)) as Texture;
+		test.tmpRect = EditorGUILayout.RectField("tmp rect title", test.tmpRect);
+		test.tmpTexture = EditorGUILayout.ObjectField("tmp texture", test.tmpTexture, typeof(Texture)) as Texture;
+	}
+}
---
+++ Assets/Scripts/Test1/Test1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+	[HideInInspector]
+	[SerializeField]
+	private Rect mRect;
+
+	public Rect MRect{
+		get{
+			return mRect;
+		}
+		set{
+			mRect = value;
+		}
+	}
+
+	[HideInInspector]
+	[SerializeField]
+	private Texture mTexture;
+
+	public Texture MTexture{
+		get{
+			return mTexture;
+		}
+		set{
+			mTexture = value;
+		}
+	}
+
+	public Rect tmpRect;
+
+	public Texture tmpTexture;
+
+	void Start()
+	{
+		Debug.Log(MRect.ToString());
+		Debug.Log(MTexture.ToString());
+		Debug.Log(tmpRect.ToString());
+		Debug.Log(tmpTexture.ToString());
+	}
+}
